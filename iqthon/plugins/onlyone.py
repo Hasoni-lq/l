f9e59224907df4ac604e15fd7e611fa738a4f594
123oqlz9q <106493352+123oqlz9q@users.noreply.github.com>
2022-05-30 00:50:59 +0300

    Create onlyone.py

---
+++ iqthon/plugins/onlyone.py
@@ -0,0 +1,69 @@
+import os
+import aiohttp
+import requests
+import random
+import re
+import time
+import sys
+import asyncio
+import math
+import heroku3
+import urllib3
+import speedtest
+import base64
+import psutil
+import platform
+from telethon.errors.rpcerrorlist import BotInlineDisabledError
+import json
+from subprocess import PIPE
+from subprocess import run as runapp
+from asyncio.exceptions import CancelledError
+from time import sleep
+from platform import python_version
+from github import Github
+from pySmartDL import SmartDL
+from pathlib import Path
+from telethon.errors import QueryIdInvalidError
+from telethon.errors import QueryIdInvalidError
+from telethon.tl.types import InputMessagesFilterDocument
+from ..core import check_owner, pool
+from datetime import datetime
+from telethon import version
+from telethon import Button, events ,types 
+from telethon.events import CallbackQuery, InlineQuery
+from telethon.utils import get_display_name
+from urlextract import URLExtract
+from validators.url import url
+from iqthon import StartTime, iqthon, catversion
+from ..Config import Config
+from ..core.logger import logging
+from ..core.managers import edit_delete, edit_or_reply
+from ..helpers.functions import catalive, check_data_base_heal_th, get_readable_time
+from ..helpers.utils import reply_id, _catutils, parse_pre, yaml_format, install_pip, get_user_from_event, _format
+from ..helpers.tools import media_type
+from . import media_type, progress
+from ..utils import load_module, remove_plugin
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from ..sql_helper.global_collection import add_to_collectionlist, del_keyword_collectionlist, get_collectionlist_items
+from . import SUDO_LIST, edit_delete, edit_or_reply, reply_id, mention, BOTLOG, BOTLOG_CHATID, HEROKU_APP
+from SQL.extras import *
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.contacts import UnblockRequest
+from telethon import client, events
+
+@iqthon.on(admin_cmd(pattern="تح(?: |$)(.*)"))    
+async def install(event):
+    if event.reply_to_msg_id:
+        try:
+            downloaded_file_name = await event.client.download_media(await event.get_reply_message(), "iqthon/plugins/")
+            if "(" not in downloaded_file_name:
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_module(shortname.replace(".py", ""))
+                await edit_delete(event, f"**- Done** `{os.path.basename(downloaded_file_name)}`", 10)
+            else:
+                os.remove(downloaded_file_name)
+                await edit_delete(event, "**  حـدث خطـأ، هـذا الملف مثبـت بالفعـل !**", 10)
+        except Exception as e:
+            await edit_delete(event, f"**♛︙  خطـأ ⚠️:**\n`{str(e)}`", 10)
+            os.remove(downloaded_file_name)
