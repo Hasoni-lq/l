8833ddbe0147443a3b38070f2a3d5e4e182c6671
123oqlz9q <106493352+123oqlz9q@users.noreply.github.com>
2022-05-30 02:42:25 +0300

    Update and rename iqthon/assistant/bot_pms.py to hsshh/assistant/bot_pms.py

--- hsshh/assistant/bot_pms.py
+++ hsshh/assistant/bot_pms.py
@@ -8,7 +8,7 @@ from telethon.errors import UserIsBlockedError
 from telethon.events import CallbackQuery, StopPropagation
 from telethon.utils import get_display_name
 
-from iqthon import Config, iqthon
+from hsshh import Config, hsshh
 
 from ..core import check_owner, pool
 from ..core.logger import logging
@@ -64,14 +64,14 @@ async def check_bot_started_users(user, event):
         await event.client.send_message(BOTLOG_CHATID, notification)
 
 
-@iqthon.bot_cmd(
+@hsshh.bot_cmd(
     pattern=f"^/start({botusername})?([\s]+)?$",
     incoming=True,
     func=lambda e: e.is_private,
 )
 async def bot_start(event):
     chat = await event.get_chat()
-    user = await iqthon.get_me()
+    user = await hsshh.get_me()
     if check_is_black_list(chat.id):
         return
     reply_to = await reply_id(event)
@@ -103,22 +103,22 @@ async def bot_start(event):
                 my_mention=my_mention,
             )
         else:
-            start_msg = f"**⌔︙ اهلا وسهلا اخوي  {mention} **\
-                        \n**⌔︙ {my_mention}'انا البوت المساعد .**\
-                        \n**⌔︙ يمكنك التواصل مع صاحب البوت فقط ارسل رسالتك 👤.**\
-                        \n\n**⌔︙ البوت خاص بسورس :** [TELETHON FOR ARABS](https://t.me/X888E)"
+            start_msg = f"**🇮🇶︙ هلا حب  {mention} **\
+                        \n**🇮🇶︙انا البوت المساعد **\
+                        \n**🇮🇶︙ يمكنك التواصل مع صاحب البوت فقط ارسل رسالتك .**\
+                        \n\n**🇮🇶︙ مطور السورس والبوت :** {my_mention}"
         buttons = [
             (
-                Button.url("تنصيب سورسنا", "https://heroku.com/deploy?template=https://github.com/Guevaras1explanations/TelethonArab-"),
+                Button.url("حسابك", "tg://settings"),
                 Button.url(
-                    "قناه المساعد",
-                    "https://t.me/E999G",
+                    "مساعدة",
+                    "https://t.me/",
                 ),
             )
         ]
     else:
-        start_msg = "**⌔︙ اهلا وسهلا بك ايها المطور ⚜️**\
-            \n⌔︙ لروئيه الاوام الخاصه بالمطور أرسل : `/مساعدة`"
+        start_msg = "**🇮🇶︙ اهلا وسهلا بك ايها المطور ⚜️**\
+            \n🇮🇶︙ لروئيه الاوام الخاصه بالمطور أرسل : `/مساعدة`"
         buttons = None
     try:
         await event.client.send_message(
@@ -139,7 +139,7 @@ async def bot_start(event):
         await check_bot_started_users(chat, event)
 
 
-@iqthon.bot_cmd(incoming=True, func=lambda e: e.is_private)
+@hsshh.bot_cmd(incoming=True, func=lambda e: e.is_private)
 async def bot_pms(event):  # sourcery no-metrics
     chat = await event.get_chat()
     if check_is_black_list(chat.id):
@@ -196,7 +196,7 @@ async def bot_pms(event):  # sourcery no-metrics
                     )
 
 
-@iqthon.bot_cmd(edited=True)
+@hsshh.bot_cmd(edited=True)
 async def bot_pms_edit(event):  # sourcery no-metrics
     chat = await event.get_chat()
     if check_is_black_list(chat.id):
@@ -289,7 +289,7 @@ async def handler(event):
                 LOGS.error(str(e))
 
 
-@iqthon.bot_cmd(
+@hsshh.bot_cmd(
     pattern=f"^/uinfo$",
     from_users=Config.OWNER_ID,
 )
@@ -342,7 +342,7 @@ async def send_flood_alert(user_) -> None:
             FloodConfig.ALERT[user_.id]["count"] = 1
         except Exception as e:
             if BOTLOG:
-                await iqthon.tgbot.send_message(
+                await hsshh.tgbot.send_message(
                     BOTLOG_CHATID, f"**Error:**\nWhile updating flood count\n`{str(e)}`"
                 )
         flood_count = FloodConfig.ALERT[user_.id]["count"]
@@ -367,7 +367,7 @@ async def send_flood_alert(user_) -> None:
                     "Is Flooding your bot !, Check `.help delsudo` to remove the user from Sudo."
                 )
                 if BOTLOG:
-                    await iqthon.tgbot.send_message(BOTLOG_CHATID, sudo_spam)
+                    await hsshh.tgbot.send_message(BOTLOG_CHATID, sudo_spam)
             else:
                 await ban_user_from_bot(
                     user_,
@@ -381,7 +381,7 @@ async def send_flood_alert(user_) -> None:
         if not fa_id:
             return
         try:
-            msg_ = await iqthon.tgbot.get_messages(BOTLOG_CHATID, fa_id)
+            msg_ = await hsshh.tgbot.get_messages(BOTLOG_CHATID, fa_id)
             if msg_.text != flood_msg:
                 await msg_.edit(flood_msg, buttons=buttons)
         except Exception as fa_id_err:
@@ -389,30 +389,30 @@ async def send_flood_alert(user_) -> None:
             return
     else:
         if BOTLOG:
-            fa_msg = await iqthon.tgbot.send_message(
+            fa_msg = await hsshh.tgbot.send_message(
                 BOTLOG_CHATID,
                 flood_msg,
                 buttons=buttons,
             )
         try:
-            chat = await iqthon.tgbot.get_entity(BOTLOG_CHATID)
-            await iqthon.tgbot.send_message(
+            chat = await hsshh.tgbot.get_entity(BOTLOG_CHATID)
+            await hsshh.tgbot.send_message(
                 Config.OWNER_ID,
                 f"⚠️  **[Bot Flood Warning !](https://t.me/c/{chat.id}/{fa_msg.id})**",
             )
         except UserIsBlockedError:
             if BOTLOG:
-                await iqthon.tgbot.send_message(BOTLOG_CHATID, "**Unblock your bot !**")
+                await hsshh.tgbot.send_message(BOTLOG_CHATID, "**Unblock your bot !**")
     if FloodConfig.ALERT[user_.id].get("fa_id") is None and fa_msg:
         FloodConfig.ALERT[user_.id]["fa_id"] = fa_msg.id
 
 
-@iqthon.tgbot.on(CallbackQuery(data=re.compile(b"bot_pm_ban_([0-9]+)")))
+@hsshh.tgbot.on(CallbackQuery(data=re.compile(b"bot_pm_ban_([0-9]+)")))
 @check_owner
 async def bot_pm_ban_cb(c_q: CallbackQuery):
     user_id = int(c_q.pattern_match.group(1))
     try:
-        user = await iqthon.get_entity(user_id)
+        user = await hsshh.get_entity(user_id)
     except Exception as e:
         await c_q.answer(f"Error:\n{str(e)}")
     else:
@@ -449,7 +449,7 @@ def is_flood(uid: int) -> Optional[bool]:
         return True
 
 
-@iqthon.tgbot.on(CallbackQuery(data=re.compile(b"toggle_bot-antiflood_off$")))
+@hsshh.tgbot.on(CallbackQuery(data=re.compile(b"toggle_bot-antiflood_off$")))
 @check_owner
 async def settings_toggle(c_q: CallbackQuery):
     if gvarstatus("bot_antif") is None:
@@ -459,8 +459,8 @@ async def settings_toggle(c_q: CallbackQuery):
     await c_q.edit("BOT_ANTIFLOOD is now disabled !")
 
 
-@iqthon.bot_cmd(incoming=True, func=lambda e: e.is_private)
-@iqthon.bot_cmd(edited=True, func=lambda e: e.is_private)
+@hsshh.bot_cmd(incoming=True, func=lambda e: e.is_private)
+@hsshh.bot_cmd(edited=True, func=lambda e: e.is_private)
 async def antif_on_msg(event):
     if gvarstatus("bot_antif") is None:
         return
